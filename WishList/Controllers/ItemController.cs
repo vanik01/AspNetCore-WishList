120decfd701985af2f7e66feeba99b25a6e7b6d0
Vandana Kacha <vandana.kacha@jci.com>
2021-10-25 22:40:06 +0530

    Add ItemController

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Model;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var item = new Item();
+            item.Id = 1;
+            item.Description = "Test";
+            _context.Items.Add(item);
+            return View("Index", _context.Items.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.Where(itm => itm.Id == id).Select(itm => itm).FirstOrDefault();
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using WishList.Model;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDbContext(DbContextOptions option) : base(option)
+        { }
+    }
+}
